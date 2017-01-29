3ef19c066d26f6c40a4ea12208c5cfd452d6b708
Propaganistas <Propaganistas@users.noreply.github.com>
2017-01-29 15:44:02 +0100

    Laravel 5.4 compatibility

--- src/TranslatableBootFormsServiceProvider.php
+++ src/TranslatableBootFormsServiceProvider.php
@@ -1,8 +1,10 @@
 <?php namespace Propaganistas\LaravelTranslatableBootForms;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
-class TranslatableBootFormsServiceProvider extends ServiceProvider {
+class TranslatableBootFormsServiceProvider extends ServiceProvider
+{
 
     /**
      * Indicates if loading of the provider is deferred.
@@ -19,7 +21,7 @@ class TranslatableBootFormsServiceProvider extends ServiceProvider {
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('translatable-bootforms.php'),
+            __DIR__ . '/../config/config.php' => config_path('translatable-bootforms.php'),
         ], 'config');
     }
 
@@ -31,23 +33,28 @@ class TranslatableBootFormsServiceProvider extends ServiceProvider {
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/config.php', 'translatable-bootforms'
+            __DIR__ . '/../config/config.php', 'translatable-bootforms'
         );
 
         // Override BootForm's form builder in order to get model binding
         // between BootForm & TranslatableBootForm working.
-        $this->app['adamwathan.form'] = $this->app->share(function ($app) {
+        $this->app->singleton('adamwathan.form', function ($app) {
             $formBuilder = new Form\FormBuilder();
             $formBuilder->setLocales($this->getLocales());
             $formBuilder->setErrorStore($app['adamwathan.form.errorstore']);
             $formBuilder->setOldInputProvider($app['adamwathan.form.oldinput']);
-            $formBuilder->setToken($app['session.store']->getToken());
+
+            $token = version_compare(Application::VERSION, '5.4', '<')
+                ? $app['session.store']->getToken()
+                : $app['session.store']->token();
+
+            $formBuilder->setToken($token);
 
             return $formBuilder;
         });
 
         // Define TranslatableBootForm.
-        $this->app['translatable-bootform'] = $this->app->share(function ($app) {
+        $this->app->singleton('translatable-bootform', function ($app) {
             $form = new TranslatableBootForm($app['bootform']);
             $form->locales($this->getLocales());
 
@@ -62,20 +69,19 @@ class TranslatableBootFormsServiceProvider extends ServiceProvider {
      */
     public function provides()
     {
-        return array(
+        return [
             'adamwathan.form',
             'translatable-bootform',
-        );
+        ];
     }
-    
+
     /**
      * Get Translatable's locales.
-     * 
+     *
      * @return array
      */
     protected function getLocales()
     {
         return with(new Translatable\TranslatableWrapper)->getLocales();
     }
-
 }
